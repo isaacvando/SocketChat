0f628037ba458bd4b892c4e930b4ff24eac1de2e
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-02-03 21:37:33 -0600

    recieve message from server

--- Client.hs
+++ Client.hs
@@ -5,7 +5,15 @@ main :: IO ()
 main = do
   putStrLn "My chat room client. Version One.\n"
   sock <- socket AF_INET Stream defaultProtocol
+  -- setSocketOption sock ReuseAddr 1
   hostAddr:_ <- getAddrInfo Nothing (Just "127.0.0.1") (Just "10746")
   connect sock (addrAddress hostAddr)
   send sock "bazbarfoo"
+  loop sock
   close sock
+
+loop :: Socket -> IO ()
+loop sock = do
+  msg <- recv sock 4096
+  print msg
+  loop sock
