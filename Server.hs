794a3d03eabad5ff8a720359bc7a7da47160f2c1
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-02-03 21:13:22 -0600

    emit message on correct host and port

--- Server.hs
+++ Server.hs
@@ -1,14 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Network.Socket
+import Network.Socket.ByteString
 import Data.List.Split (splitOn)
 
 main :: IO ()
 main = do
   users <- parseUsers <$> readFile "users.txt"
   sock <- socket AF_INET Stream defaultProtocol
-  hostAddr:_ <- getAddrInfo Nothing (Just "127.0.0.1") Nothing
+  hostAddr:_ <- getAddrInfo Nothing (Just "127.0.0.1") (Just "10746")
+  print $ addrAddress hostAddr
   bind sock (addrAddress hostAddr)
-  print sock
-  close sock
+  listen sock 1
+  loop sock
+
+loop :: Socket -> IO ()
+loop sock = do
+  (sock', _) <- accept sock
+  send sock' "foobarbaz\n"
+  close sock'
+  loop sock
 
 parseUsers :: String -> [(String, String)]
 parseUsers xs = map f (lines xs)
