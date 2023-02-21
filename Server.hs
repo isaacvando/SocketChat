b303a475133e6a6ffa29531b7716b1f9dc7e450a
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-02-20 21:15:17 -0600

    refactor

--- Server.hs
+++ Server.hs
@@ -5,12 +5,29 @@ import Data.List.Split (splitOn)
 main :: IO ()
 main = do
   users <- parseUsers <$> readFile "users.txt"
+  sock <- getSock
+  runServer sock server
+
+runServer :: Socket -> (Socket -> IO ()) -> IO ()
+runServer s server = do
+  server s
+  runServer s server
+
+server :: Socket -> IO ()
+server sock = do
+  (conn,_) <- accept sock
+  recv conn 4096 >>= print
+  close conn
+
+getSock :: IO Socket
+getSock = do
   sock <- socket AF_INET Stream defaultProtocol
   hostAddr:_ <- getAddrInfo Nothing (Just "127.0.0.1") (Just "10746")
-  print $ addrAddress hostAddr
+  -- print $ addrAddress hostAddr
   bind sock (addrAddress hostAddr)
   listen sock 1
-  loop sock
+  return sock
+
 
 loop :: Socket -> IO ()
 loop sock = do
