ce7ae06525a830da78c9a7c2e6a1bfae108ee7f5
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-02-21 20:12:57 -0600

    main functionality

--- Server.hs
+++ Server.hs
@@ -1,18 +1,20 @@
 import Network.Socket
-import Network.Socket.ByteString
+import Network.Socket.ByteString (send, recv)
 import Data.List.Split (splitOn)
 import qualified Control.Exception as E
 -- import Data.Maybe (isJust)
 import qualified Data.ByteString.UTF8 as U
+import System.IO.Strict as S (readFile)
+import Control.Monad (unless)
 
 data State = State { loggedIn :: Maybe String, users :: [(String,String)]}
 
 main :: IO ()
 main = do
-  users <- parseUsers <$> readFile "users.txt"
+  users <- parseUsers <$> S.readFile "users.txt"
   putStrLn "My chat room server. Version One."
   let state = State {loggedIn = Nothing, users = users}
-  E.bracket getSock close (runServer server state)
+  _ <- E.bracket getSock close (runServer server state)
   return ()
 
 
@@ -21,21 +23,52 @@ runServer server state sock = do
   state' <- server state sock
   runServer server state' sock
 
+
 server :: State -> Socket -> IO State
 server state sock = do
   (conn,_) <- accept sock
   msg <- recv conn 4096
-  state' <- process (U.toString msg) conn
+  let (state', resp, echo) = process state (U.toString msg)
+  _ <- send conn (U.fromString resp)
+  unless (null echo) $ putStrLn echo
   close conn
+  write $ renderUsers state'
   return state'
   where
-    process msg conn = case (loggedIn state, words msg) of 
-      (Just name, "send":xs) -> talk conn (name ++ ": " ++ unwords xs) >> return state
-      (Nothing, "send":_) -> talk conn ("Denied. Please login first.") >> return state
-      (_, xs) -> send conn (U.fromString $ ("\"" ++ unwords xs ++ "\" is not a valid command.")) >> return state
+    write xs =  writeFile "users.txt" xs
 
-talk :: Socket -> String -> IO Int
-talk conn msg = send conn (U.fromString msg)
+
+process :: State -> String -> (State, String, String)
+process st msg = case (loggedIn st, words msg) of
+  (Just name, "send":xs) -> 
+    let reply = name ++ ": " ++ unwords xs 
+    in (st, reply, reply)
+  (Nothing, "send":_) -> 
+    (st, "Denied. Please login first.", "")
+
+  (_, ["newuser", name, pass]) -> if (name, pass) `elem` users st 
+    then (st, "Denied. User account already exists.", "")
+    else (st {users = (name, pass) : users st}, "New user account created. Please login.", "New user account created.")
+
+  (Just name, "login":_) -> 
+    (st, "Denied. User " ++ name ++ " is already logged in.", "")
+  (Nothing, ["login", name, pass]) -> if (name,pass) `elem` (users st)
+    then (st {loggedIn = Just name}, "login confirmed", name ++ " login.")
+    else (st, "Denied. User name or password incorrect.", "")
+
+  (Just name, ["logout"]) -> 
+    (st {loggedIn = Nothing}, name ++ " left.", name ++ " logout.")
+  (Nothing, ["logout"]) -> 
+    (st, "No user to logout.", "")
+
+  (_, xs) -> (st, "\"" ++ unwords xs ++ "\" is not a valid command.", "")
+
+
+renderUsers :: State -> String
+renderUsers = unlines . map go . users
+  where
+    go (name,pass) = "(" ++ name ++ ", " ++ pass ++ ")"
+
 
 getSock :: IO Socket
 getSock = do
@@ -45,6 +78,7 @@ getSock = do
   bind sock (addrAddress hostAddr)
   listen sock 1
   return sock
+
 
 parseUsers :: String -> [(String, String)]
 parseUsers xs = map f (lines xs)
